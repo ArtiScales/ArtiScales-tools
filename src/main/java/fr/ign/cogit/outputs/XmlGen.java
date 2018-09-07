bab6a2e2ffa0f04ef23805d58b45e698e8d641aa
maxcolomb <colomb.maxime@gmail.com>
2018-09-07 15:35:55 +0200

    xml output

---
+++ src/main/java/fr/ign/cogit/outputs/XmlGen.java
@@ -0,0 +1,44 @@
+package fr.ign.cogit.outputs;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class XmlGen {
+
+	File fileName;
+
+	public XmlGen(File filename, String scenarName) throws IOException {
+		fileName = filename;
+		FileWriter writer = new FileWriter(fileName, false);
+		writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+		writer.append("\n");
+		beginBalise(scenarName);
+		writer.close();
+	}
+
+	public void addLine(String balise, String argument) throws IOException {
+		FileWriter writer = new FileWriter(fileName, true);
+		String line = "<" + balise + ">" + argument + "</" + balise + ">";
+		writer.append(line + "\n");
+		writer.close();
+	}
+
+	public void beginBalise(String balise) throws IOException {
+		FileWriter writer = new FileWriter(fileName, true);
+		String line = "<" + balise + ">";
+		writer.append(line + "\n");
+		writer.close();
+	}
+
+	public void endBalise(String balise) throws IOException {
+		FileWriter writer = new FileWriter(fileName, true);
+		String line = "</" + balise + ">";
+		writer.append(line + "\n");
+		writer.close();
+	}
+
+	public void close(String balise) {
+
+	}
+}
